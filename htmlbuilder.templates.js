502c24032185db67decee6a0849db227931f08aa
Alex Dorey <dorey415@gmail.com>
2010-10-04 00:07:10 -0400

    Updated to include basic "templates" and now using latest version of buildin.js. Uses string concatenation behind the scenes. No longer dependent on htmlbuilder (though using compatible syntax.)

---
+++ htmlbuilder.templates.js
@@ -0,0 +1,81 @@
+/* htmlbuilder templates
+ * -- uses htmlbuilder's syntax to generate arrays that can 
+ *    be converted into commonly used HTML structures
+ * -- Purpose:
+ *    <table><tbody><tr><td>This is so much work!</td></tr></tbody></table>
+ * -- original examples include: table & page
+ *
+ * Usage:
+ *   builder.templates.table(
+ *     {<options>}, //<==options are optional
+ *     [<th 1 contents>, <th 2 contents>],
+ *     [<td 1 contents>, <td 2 contents>]
+ *   )
+ *
+ * Example:
+ *       var table = builder.templates.table(['header1','header2'], ['item1', 'item2'])
+ *
+ * This will give you a htmlbuilderable array:
+ *      ['table', {}, 
+ *          ['tbody', {},
+ *              ['tr', {},
+ *                  ['th', {}, 'header1'],
+ *                  ['th', {}, 'header2']
+ *              ],
+ *              ['tr', {},
+ *                  ['td', {}, 'item1'],
+ *                  ['td', {}, 'item2']
+ *              ]
+ *          ]
+ *      ]
+ *
+ * Which you can build and insert!
+ *       document.body.appendChild(builder.build(table))
+ */
+;if(typeof(window.builder)=='undefined'){window.builder={}}
+
+builder.templatesVersion = "0.0.2";
+
+builder.templates=(function(){
+	function TableTemplate(options) {
+		var rowCount, val, _currentRow, colIndex,
+			_optsPresent = !((options instanceof Array) && (typeof(options)!=='string')),
+			opts = _optsPresent ? options : {},
+			argCountRowStart = _optsPresent ? 1 : 0, //skips first argument if _optsPresent
+			useTHs = (opts.th || true),
+			_contents = ["tbody", {}];
+		
+		for(rowCount = argCountRowStart; rowCount<arguments.length; rowCount++) {
+			val=arguments[rowCount];
+			_currentRow = ["tr", {}];
+			for(colIndex=0; colIndex<val.length; colIndex++) {
+				_currentRow.push([
+					(useTHs && rowCount==argCountRowStart) ? "th" : "td",
+					{},
+					val[colIndex]
+				]);
+			}
+			_contents.push(_currentRow);
+		}
+		return [
+			"table", {},
+			_contents
+		];
+	}
+	
+	function BasicHTMLPage(opts) {
+		return [
+			["head", {},
+				["title", {}, opts.title]
+			],
+			["body", {}, 
+				opts.body
+			]
+		]
+	}
+	
+	return {
+		table: TableTemplate,
+		page: BasicHTMLPage
+	}
+})();
---
+++ jquery.buildin.js
@@ -0,0 +1,73 @@
+/*-- jQuery Plug-in: "buildIn"
+----  (author:Alex Dorey <http://github.com/dorey>)
+---- =========================
+---- > Based on the JSON syntax of the htmlbuilder script from indieinvader
+---- > Allows jQuery enabled pages to describe html in an array syntax
+---- > Defaults to using jQuery's "append" function, but can also use "prepend", "before", and "after"
+---- > Also allows "replace", which is equivalent to $(selector).empty().append(...)
+----
+---- Basic usage:
+----   $(selector).buildIn(<tagName>, <tagAttributes>, <elementContents>)
+----   example:
+----     $('body').buildIn('a', {href:'#simple'}, 'A link is easy to add!')
+----
+--*/
+;
+(function($){
+	function validateJson2HtmlArgument(argNum, arg) {
+		//some validation could go here...
+		return [arg]; //i didn't realize $(...).map(...) would flatten a shallow array
+	}
+	function json2html(){
+		if(arguments.length==1) {
+			if(typeof(arguments[0])==='string') {return arguments[0] }
+			if(arguments[0] instanceof Array) {return json2html.apply(this, arguments[0])}
+			return "";
+		}
+		if(arguments[1] instanceof Array) {
+			//a temporary shortcut, but this means that the params are not in the standard format--
+			// ("str", {obj}, <etc>*)
+			//as such, it will process each item and join the results
+			var i, concatted = "";
+			for(i=0;i<arguments.length;i++) {concatted+=json2html(arguments[i])}
+			return concatted;
+		}
+		var args = $(arguments).map(validateJson2HtmlArgument),
+			tag = args[0],
+			output="<" + tag;
+		
+		if (typeof(args[1])==='object' &&
+		 	args[1] !== null) {
+			$.each(args[1], function(k,v){
+				output+=' '+k+'="'+v+'"';
+			});
+		}
+		
+		if(args.length<3) { //probably should use a better condition here
+			return output+" />";
+		}
+		output+=">";
+
+		for(var i=2;i<args.length; i++) {
+			output+=json2html(args[i]);
+		}
+		output+="</"+tag+">";
+		return output;
+	}
+	function json2dom(){
+		return $(json2html.apply(this,arguments))
+	}
+	var jQueryInserters = /append|prepend|before|after|replace/i;
+	function buildIn(){
+		var args = arguments, action = 'append';
+		if(typeof(arguments[0])==='string' && arguments[0].match(jQueryInserters)) {
+			args = $(arguments).splice(1);
+			action = arguments[0].match(jQueryInserters)[0].toLowerCase();
+			if(action=='replace'){action='html';}
+		}
+		eval("this."+action+"(json2html.apply(this,args))");
+	}
+	$.json2html = json2html;
+	$.json2dom = json2dom;
+	$.fn.buildIn = buildIn;
+})(jQuery);
