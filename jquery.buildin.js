99f0af8c25e1dc0714e23afd589dd9d48235c6bf
Alex Dorey <dorey415@gmail.com>
2010-10-23 15:06:04 -0400

    taking out some clutter

--- jquery.buildin.js
+++ jquery.buildin.js
@@ -1,55 +1,42 @@
-/*-- jQuery Plug-in: "buildIn"
-----  (author:Alex Dorey <http://github.com/dorey>)
----- =========================
----- > Based on the JSON syntax of indieinvader's htmlbuilder and JsonML.
----- > Allows jQuery enabled pages to describe html in an array syntax
----- > Defaults to using jQuery's "append" function, but can also use "prepend", "before", and "after"
----- > Also allows "replace", which is equivalent to $(selector).innerHTML="<element tree />"
-----
----- Basic usage:
-----   $(selector).buildIn(<tagName>, <tagAttributes>, <elementContents>)
-----   example:
-----     $('body').buildIn('a', {href:'#simple'}, 'A link is easy to add!')
-----
---*/
+/*!   jQuery Plug-in: "buildIn"
+-*    (author:Alex Dorey <http://github.com/dorey>)
+-*   =========================
+-*   > Based on the JSON syntax of indieinvader's htmlbuilder and JsonML.
+-*   > Allows jQuery enabled pages to describe html in an array syntax
+-*   > Defaults to using jQuery's "append" function, but can also use "prepend", "before", and "after"
+-*   > Also allows "replace", which is equivalent to $(selector).innerHTML="<element tree />"
+-*  
+-*   Basic usage:
+-*     $(selector).buildIn(<tagName>, <tagAttributes>, <elementContents>)
+-*     example:
+-*       $('body').buildIn('a', {href:'#simple'}, 'A link is easy to add!')
+-*  
+*/
 ;
 (function($){
-	function validateJson2HtmlArgument(argNum, arg) {
-		//some validation could go here...
-		return [arg]; //i didn't realize $(...).map(...) would flatten a shallow array
-	}
 	function json2html(){
 		if(arguments.length==1) {
 			if(typeof(arguments[0])==='string') {return arguments[0] }
 			if(arguments[0] instanceof Array) {return json2html.apply(this, arguments[0])}
 			return "";
 		}
-		if(arguments[1] instanceof Array) {
-			//a temporary shortcut, but this means that the params are not in the standard format--
-			// ("str", {obj}, <etc>*)
-			//as such, it will process each item and join the results
-			var i, concatted = "";
-			for(i=0;i<arguments.length;i++) {concatted+=json2html(arguments[i])}
-			return concatted;
-		}
-		var args = $(arguments).map(validateJson2HtmlArgument),
-			tag = args[0],
+		var tag = arguments[0],
 			output="<" + tag;
 		
-		if (typeof(args[1])==='object' &&
-		 	args[1] !== null) {
-			$.each(args[1], function(k,v){
+		if (typeof(arguments[1])==='object' &&
+		 	arguments[1] !== null) {
+			$.each(arguments[1], function(k,v){
 				output+=' '+k+'="'+v+'"';
 			});
 		}
 		
-		if(args.length<3) { //probably should use a better condition here
+		if(arguments.length<3) { //probably should use a better condition here
 			return output+" />";
 		}
 		output+=">";
 
-		for(var i=2;i<args.length; i++) {
-			output+=json2html(args[i]);
+		for(var i=2;i<arguments.length; i++) {
+			output+=json2html(arguments[i]);
 		}
 		output+="</"+tag+">";
 		return output;
